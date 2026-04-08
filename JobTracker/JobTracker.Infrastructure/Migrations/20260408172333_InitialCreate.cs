bf7445f75f7cc579de40d7674edefc05b263340a
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2026-04-08 22:57:32 +0530

    added migration and updated database

---
+++ JobTracker/JobTracker.Infrastructure/Migrations/20260408172333_InitialCreate.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace JobTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "job_applications",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    company_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    job_title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    job_description = table.Column<string>(type: "text", nullable: true),
+                    job_posting_url = table.Column<string>(type: "text", nullable: true),
+                    salary_range = table.Column<string>(type: "text", nullable: true),
+                    status = table.Column<int>(type: "integer", nullable: true),
+                    date_applied = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    notes = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_job_applications", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "contacts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    full_name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    role = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true),
+                    linked_in_url = table.Column<string>(type: "text", nullable: true),
+                    job_application_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_contacts", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_contacts_job_applications_job_application_id",
+                        column: x => x.job_application_id,
+                        principalTable: "job_applications",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "interviews",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    job_application_id = table.Column<int>(type: "integer", nullable: true),
+                    interview_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    type = table.Column<int>(type: "integer", nullable: true),
+                    interviewer_name = table.Column<string>(type: "text", nullable: true),
+                    notes = table.Column<string>(type: "text", nullable: true),
+                    meeting_link = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_interviews", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_interviews_job_applications_job_application_id",
+                        column: x => x.job_application_id,
+                        principalTable: "job_applications",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_contacts_job_application_id",
+                table: "contacts",
+                column: "job_application_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_interviews_job_application_id",
+                table: "interviews",
+                column: "job_application_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contacts");
+
+            migrationBuilder.DropTable(
+                name: "interviews");
+
+            migrationBuilder.DropTable(
+                name: "job_applications");
+        }
+    }
+}
