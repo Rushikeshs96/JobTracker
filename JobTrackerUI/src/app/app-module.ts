c5836084bfd1a2aa18d80d26200c3af84a525e0a
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2026-04-11 23:58:00 +0530

    dashboard compoent fix and missing configs fixed

--- JobTrackerUI/src/app/app-module.ts
+++ JobTrackerUI/src/app/app-module.ts
@@ -2,26 +2,26 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
+
 import { App } from './app';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Proactiveservice } from './proactiveservice/proactiveservice';
-import { Dashboard } from './components/dashboard/dashboard';
-import { JobList } from './components/job-list/job-list';
-import { JobForm } from './components/job-form/job-form';
-import { Navbar } from './components/navbar/navbar';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatCardModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
